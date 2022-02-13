6035d17e88cc44d2b5dd0afd161db542381037e7
Deck Stream <tneswick2@verizon.net>
2022-02-12 18:03:57 -0800

    added randomNumber function and selections of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min) + min);
+
+  return value;
+};
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var generatePassword = function() {
+  var chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var chars2 = chars1.toLowerCase;
+  var chars3 = "0123456789";
+  var chars4 = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"'
+  var passLength = prompt("Select a password length: Mininum 8, Maximum 128.");
+  passLength = parseInt(passLength);
+  var passTypeUpper = confirm("Do you want to include UPPERCASE letters?");
+  if (passTypeUpper) {
+    charset = chars1
+  };
+  var passTypeLower = confirm("Do you want to include lowercase letters?");
+  if (passTypeLower) {
+    charset = charset + chars2
+  };
+  var passTypeNum = confirm("Do you want to include numbers?");
+  if (passTypeNum) {
+    charset = charset + chars3
+  }
+  var passTypeSpec = confirm("Do you want to include special characters?");
+    if(passTypeSpec) {
+      charset = charset + chars4
+    }
+  console.log(charset)
+  return passLength + passTypeUpper + passTypeLower + passTypeNum + passTypeSpec;
+};
